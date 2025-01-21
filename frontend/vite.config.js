fabacc33e125db33d4ba263b244b852b06e8be24
Konstantin <k.gromakovskiy@yandex.ru>
2025-01-21 14:54:05 +0300

    setting up the production launch

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const port  = process.env.PORT || 3000
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server:{
     open:true,
     host: '0.0.0.0',
-    port:10000,
+    port:port,
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
