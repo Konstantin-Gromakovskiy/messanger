b420d70982972e998ddf95398147b8dd065a2158
Konstantin <k.gromakovskiy@yandex.ru>
2025-02-20 12:56:18 +0300

    refactoring 2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,5 +2,9 @@ import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import init from './init.jsx';
 
-const vdom = await init();
-createRoot(document.getElementById('root')).render(vdom);
+const runApp = async () => {
+  const vdom = await init();
+  createRoot(document.getElementById('root')).render(vdom);
+};
+
+runApp();
